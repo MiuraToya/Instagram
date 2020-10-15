15bb273686a8d61d960886aa505a77ed2ec0e131
Miura Toya <dengzais8@gmail.com>
2020-10-16 00:22:27 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 三浦　登哉 on 2020/10/16.
+//  Copyright © 2020 toya.miura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
