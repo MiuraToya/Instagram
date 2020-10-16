8fc815dece39598d1d2939ea39c4453adbfaf6e1
Miura Toya <dengzais8@gmail.com>
2020-10-16 15:09:53 +0900

    podfile,pod,gitignoreをコミット

---
+++ Instagram/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  Instagram
+//
+//  Created by 三浦　登哉 on 2020/10/16.
+//  Copyright © 2020 toya.miura. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
