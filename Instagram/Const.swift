8dcf6efccb9250d124b1df2ab0e651ae7c72cb3b
Miura Toya <dengzais8@gmail.com>
2020-10-22 13:27:19 +0900

    構造体を作成

---
+++ Instagram/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 三浦　登哉 on 2020/10/18.
+//  Copyright © 2020 toya.miura. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+   
+}
